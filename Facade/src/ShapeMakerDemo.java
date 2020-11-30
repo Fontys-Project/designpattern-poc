fd56ca9815819806d09eda8f9a5cb5a28245879b
Mycha <m.devrees@gmail.com>
2020-11-30 20:07:39 +0100

    add Facade demo

---
+++ Facade/src/ShapeMakerDemo.java
@@ -0,0 +1,9 @@
+public class ShapeMakerDemo {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
